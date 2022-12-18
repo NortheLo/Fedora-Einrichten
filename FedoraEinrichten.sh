1b239976efbcf50c058ec8acac612e1870655ef6
NortheLo <19312092+NortheLo@users.noreply.github.com>
2022-12-18 23:42:06 +0100

    Update FedoraEinrichten.sh
    Enabling and starting SSH deamon

--- FedoraEinrichten.sh
+++ FedoraEinrichten.sh
@@ -11,6 +11,8 @@ if [ "$EUID" == 0 ]
 	mkdir ~/Dokumente/Schematics
 	mkdir ~/Dokumente/Programming/Rust
 	mkdir ~/Dokumente/Programming/Shell
+	systemctl enable sshd
+	systemctl start sshd
 
 	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
