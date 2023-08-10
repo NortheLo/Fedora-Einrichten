4e731290ee4d2735986426c42bc0c0b247c0c9d9
NortheLo <19312092+NortheLo@users.noreply.github.com>
2023-08-10 16:34:02 +0200

    Update FedoraEinrichten.sh
    Added tmux and coc configs; Added cleanup of vimrc

--- FedoraEinrichten.sh
+++ FedoraEinrichten.sh
@@ -5,16 +5,19 @@ if [ "$EUID" == 0 ]
 	rpm --import https://packages.microsoft.com/keys/microsoft.asc
 	sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 	dnf install gparted code gfortran neovim gnome-tweak-tool gnome-extensions-app kernel-devel gcc g++ alacarte virt-manager kicad kicad-packages3d lm_sensors -y 
-	mkdir ~/Dokumente/Programming 
- 	mkdir ~/Dokumente/Programming/C++ ~/Dokumente/Programming/C ~/Dokumente/Schematics ~/Dokumente/Programming/Shell
+	mkdir ~/Documents/Programming 
+ 	mkdir ~/Dosuments/Programming/C++ ~/Documents/Programming/C ~/Documents/Schematics ~/Documents/Programming/Shell
 	systemctl enable sshd
 	systemctl start sshd
 
 	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-	mkdir ~/.config/nvim
+	mkdir ~/.config/nvim  ~/.config/coc
 	git clone https://github.com/NortheLo/vimrc
-	mv ../vimrc/init.vim ~/.config/nvim/
+	mv vimrc/init.vim ~/.config/nvim/
+ 	mv vimrc/coc-settings.json ~/.config/coc
+  	mv vimrc/.tmux.conf ~/.tmux.conf
+   	rm -r vimrc
 
 	echo "Everything should be set up now!" && reboot
 fi
