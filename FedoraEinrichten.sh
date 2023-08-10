61dde94abd3cf43500033ce9b2fee06f364b8db9
NortheLo <19312092+NortheLo@users.noreply.github.com>
2023-08-10 16:25:46 +0200

    Update FedoraEinrichten.sh

--- FedoraEinrichten.sh
+++ FedoraEinrichten.sh
@@ -5,12 +5,8 @@ if [ "$EUID" == 0 ]
 	rpm --import https://packages.microsoft.com/keys/microsoft.asc
 	sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 	dnf install gparted code gfortran neovim gnome-tweak-tool gnome-extensions-app kernel-devel gcc g++ alacarte virt-manager kicad kicad-packages3d lm_sensors -y 
-	mkdir ~/Dokumente/Programming
-	mkdir ~/Dokumente/Programming/C++
-	mkdir ~/Dokumente/Programming/C
-	mkdir ~/Dokumente/Schematics
-	mkdir ~/Dokumente/Programming/Rust
-	mkdir ~/Dokumente/Programming/Shell
+	mkdir ~/Dokumente/Programming 
+ 	mkdir ~/Dokumente/Programming/C++ ~/Dokumente/Programming/C ~/Dokumente/Schematics ~/Dokumente/Programming/Shell
 	systemctl enable sshd
 	systemctl start sshd
 
